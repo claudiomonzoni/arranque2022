b31aa7fbd9834264f1101baa20c2075a6cba87ed
claudiomonzoni <claudiomonzoni@hotmail.com>
2022-09-13 17:23:40 -0500

    ya me logeo con github

--- pages/fire.jsx
+++ pages/fire.jsx
@@ -1,12 +1,46 @@
 import Layout from "../components/Layout";
-import  StyledFirebaseAuth  from "react-firebaseui/StyledFirebaseAuth";
+import StyledFirebaseAuth from "react-firebaseui/StyledFirebaseAuth";
 import { app } from "../firebase";
-import {getAuth}  from  "firebase/auth";
+import { getAuth, onAuthStateChanged, GithubAuthProvider, signInWithPopup  } from "firebase/auth";
+// import { getFirestore, collections } from "firebase/firestorre"
 
 export default function fire() {
- const auth = getAuth(app);
- console.log(auth)
- // seguir este tuto: https://www.youtube.com/watch?v=rQvOAnNvcNQ&t=224s
+
+  const auth = getAuth(app);
+  const logearse = () =>{
+    const provider = new GithubAuthProvider();
+    
+    signInWithPopup(auth, provider)
+    .then((res) => {
+      const credential = GithubAuthProvider.credentialFromResult(res);
+      const token = credential.accessToken;
+       // The signed-in user info.
+    const user = res.user;
+      console.log(user)
+    }).catch((error) => {
+      // Handle Errors here.
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      // The email of the user's account used.
+      const email = error.customData.email;
+      // The AuthCredential type that was used.
+      const credential = GithubAuthProvider.credentialFromError(error);
+      // ...
+    });
+  }
+  // const db = getFirestore(app)
+  // const todoCollection = collections(db, "todo")
+
+  //detectar si el usuario esta logeado
+  onAuthStateChanged(auth, (user) => {
+    if (user == null) {
+      console.log("no hay usuario");
+    } else {
+      console.log("logeado");
+    }
+  });
+
+  // seguir este tuto: https://www.youtube.com/watch?v=rQvOAnNvcNQ&t=224s
   // const uiConfig = {
   //   singInSuccessUrl: "/",
   //   singInOptions: [app.auth.GithubAuthProvider.PROVIDER_ID]
@@ -17,6 +51,7 @@ export default function fire() {
       <div>
         <h1>Aqui las pruebas de firebase</h1>
         <p>empezamos con el login</p>
+        <bottom className="btn" onClick={()=>{logearse()}}>Logearse</bottom>
         {/* <StyledFirebaseAuth uiConfig={uiConfig} firebase={firebase.auth} /> */}
       </div>
     </Layout>
